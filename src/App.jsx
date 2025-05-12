149cf69650ebb9bc5d67c528ee4569ea8b62c866
Abir Chowdhury <abirchowdhury0928@gmail.com>
2025-05-12 17:13:36 +0600

    Complate hero sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Header from "./components/Header"
+import Hero from "./components/Hero"
 
 function App() {
 
   return (
     <>
       <Header></Header>
+      <main>
+        <Hero></Hero>
+      </main>
     </>
   )
 }
