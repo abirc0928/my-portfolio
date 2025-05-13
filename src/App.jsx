61cbad2c35eb9f7d60a7d4f6c13772a761c4ee22
Abir Chowdhury <abirchowdhury0928@gmail.com>
2025-05-13 17:54:34 +0600

    Added About, tools, projects sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
+import About from "./components/About"
 import Header from "./components/Header"
 import Hero from "./components/Hero"
+import Review from "./components/Review"
+import Skill from "./components/Skill"
+import Work from "./components/Work"
 
 function App() {
+
 
   return (
     <>
       <Header></Header>
       <main>
         <Hero></Hero>
+        <About></About>
+        <Skill></Skill>
+        <Work></Work>
+        <Review></Review>
       </main>
     </>
   )
